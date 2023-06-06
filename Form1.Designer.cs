6fa771a29159105faad290be46b8fb9081729f16
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-06-06 06:27:09 +0300

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Program__21
+﻿namespace ОАИП_12
 {
     partial class Form1
     {
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Пароль";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Почта";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(71, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 31);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Авторизация";
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(103, 79);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(175, 27);
+            this.textBox_email.TabIndex = 17;
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(103, 128);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.Size = new System.Drawing.Size(175, 27);
+            this.textBox_pass.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 29);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 29);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Регистрация";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(312, 296);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox_email;
+        private TextBox textBox_pass;
+        private Button button1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,58 @@
-namespace Program__21
+namespace ОАИП_12
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //AppDbContext con=new AppDbContext();
+            //con.Database.EnsureDeleted();
+            //con.Database.EnsureCreated();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Client user;
+            using (var context = new AppDbContext())
+            {
+                var email = textBox_email.Text;
+                var password = textBox_pass.Text;
+
+                user = context.Clients.FirstOrDefault(u => u.Email == email && u.Password == password);
+            }
+
+
+            if (user != null)
+            {
+                MessageBox.Show("Вы успешно авторизовались!", "Авторизация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MainForm mainForm = new MainForm(new AppDbContext(), user);
+                Hide();
+                mainForm.ShowDialog();
+
+                DialogResult dialogResult = MessageBox.Show("Закрыть программу?", "Думай", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                    Close();
+                else if (dialogResult == DialogResult.No)
+                    Show();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный email или пароль", "Ошибка авторизации", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Regestr registrationForm = new Regestr(new AppDbContext());
+            Hide();
+            registrationForm.ShowDialog();
+            Show();
         }
     }
 }
