7e9d4c4f24d0cc3c09bfdbfe9e651fc0698a3d6d
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-06-06 06:22:57 +0300

    Добавьте файлы проекта.

---
+++ AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОАИП_12
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=MSI;Initial Catalog=Laba12;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasMany(u => u.Sales)
+                .WithOne(b => b.Client)
+                .HasForeignKey(b => b.Clientid);
+        }
+
+    }
+}
---
+++ Regestr.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ОАИП_12
+{
+    public partial class Regestr : Form
+    {
+        public Regestr()
+        {
+            InitializeComponent();
+        }
+
+        private void Regestr_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private readonly AppDbContext _context;
+        public Regestr(AppDbContext context)
+        {
+            InitializeComponent();
+            _context = context;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var user = new Client
+            {
+                Name = textBox_name.Text,
+                Email = textBox_email.Text,
+                Phone = textBox_phone.Text,
+                Password = textBox_pass.Text,
+               
+            };
+
+            _context.Clients.Add(user);
+            _context.SaveChanges();
+
+            MessageBox.Show("Вы успешно зарегистрировались!", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+
+
+    }
+
+}
