57a4200e522c1de82d8ca0ce108562df507790c5
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-06-06 06:27:36 +0300

    Основная форма

---
+++ MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ОАИП_12
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_del = new System.Windows.Forms.Button();
+            this.button_ex = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(397, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(448, 30);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(94, 29);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "добавить";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_del
+            // 
+            this.button_del.Location = new System.Drawing.Point(448, 81);
+            this.button_del.Name = "button_del";
+            this.button_del.Size = new System.Drawing.Size(94, 29);
+            this.button_del.TabIndex = 2;
+            this.button_del.Text = "удалить";
+            this.button_del.UseVisualStyleBackColor = true;
+            this.button_del.Click += new System.EventHandler(this.button_del_Click);
+            // 
+            // button_ex
+            // 
+            this.button_ex.Location = new System.Drawing.Point(448, 189);
+            this.button_ex.Name = "button_ex";
+            this.button_ex.Size = new System.Drawing.Size(94, 29);
+            this.button_ex.TabIndex = 3;
+            this.button_ex.Text = "выход";
+            this.button_ex.UseVisualStyleBackColor = true;
+            this.button_ex.Click += new System.EventHandler(this.button_ex_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 269);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Статус:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 333);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_ex);
+            this.Controls.Add(this.button_del);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button_add;
+        private Button button_del;
+        private Button button_ex;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label label1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ОАИП_12
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+           
+        }
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private readonly AppDbContext _context;
+        private readonly Client _user;
+
+        public MainForm(AppDbContext context, Client user)
+        {
+            InitializeComponent();
+            cn = new SqlConnection(@"Data Source=MSI;Initial Catalog=Laba12;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            cn.Open();
+
+            _context = context;
+            _user = user;
+
+            _user.Sales = _context.Sales.Where(b => b.Clientid == _user.Id).ToList();
+
+
+            Text = $"Покупки пользователя {_user.Name}";
+
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = _user.Sales;
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var saleForm = new SaleForm(_context, _user);
+                if (saleForm.ShowDialog() == DialogResult.OK)
+                {
+                    _user.Sales.Add(saleForm.Sale);
+                    _context.SaveChanges();
+
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = _user.Sales;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Выберите строчку!"); }
+
+        }
+
+        private void button_del_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                cmd = new SqlCommand("DELETE FROM Sales where Id='" + Convert.ToString(selectedRow) + "'", cn);
+                dr = cmd.ExecuteReader();
+                var sale = (Sale)selectedRow.DataBoundItem;
+                _user.Sales.Remove(sale);
+                _context.SaveChanges();
+
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = _user.Sales;
+            }
+            catch (Exception ex) { MessageBox.Show("Выберите строчку!"); }
+        }
+
+        private void button_ex_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                var sale = (Sale)selectedRow.DataBoundItem;
+                Sale salev = _context.Sales.FirstOrDefault(u => u.Id == sale.Id);
+
+                if (salev != null) salev.Status = comboBox1.SelectedItem.ToString();
+                _context.SaveChanges();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = _user.Sales;
+            }
+            catch (Exception ex) { MessageBox.Show("Выберите строчку!"); }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new string[] { "Оплачен", "В пути", "Готов к выдаче", "Получен" });
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
+
