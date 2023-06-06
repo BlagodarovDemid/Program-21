b9029dde52ac7f20ec0172e4400a5e6c3e9b09e7
BlagodarovDemid <d.a.Blagodarov@gmail.com>
2023-06-06 06:26:43 +0300

    Клиент

---
+++ Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОАИП_12
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public List<Sale> Sales { get; set; }
+        public Client()
+        {
+            Sales = new List<Sale>();
+        }
+
+    }
+}
